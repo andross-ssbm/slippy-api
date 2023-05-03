fb318354b9eb1b259192e6f637e2461883baa33e
Sophia <consts7s@gmail.com>
2023-05-03 22:07:38 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='slippy_api',
+    version='0.1',
+    author='Sophia',
+    author_email='sophimander@gmail.com',
+    description='API wrapper for slippi.gg',
+    long_description='Simple wrapper for the GraphQL api on slippi.gg',
+    long_description_content_type='text/markdown',
+    url='https://github.com/ConstObject/slippy-api',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
