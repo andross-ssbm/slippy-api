acf40f78bf8bbc3fc6aa8ebf036db662d0c4f11f
Sophia <consts7s@gmail.com>
2023-05-16 14:55:58 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='slippy_api',
-    version='0.2',
+    version='0.3',
     author='Sophia',
     author_email='sophimander@gmail.com',
     description='API wrapper for slippi.gg',
